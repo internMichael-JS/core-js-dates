afba8428fae8110a9ac5a6f82c55d997d6f5b73e
Michail <astapenko.m99@gmail.com>
2025-03-05 18:11:00 +0300

    feat: done function getTime and dateToTimestamp

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,10 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  const staticDate = new Date(Date.UTC(1970, 0, 1));
+  const newDate = new Date(date);
+  return newDate - staticDate;
 }
 
 /**
@@ -31,8 +33,9 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const time = new Date(date);
+  return `${String(time.getHours()).padStart(2, 0)}:${String(time.getMinutes()).padStart(2, 0)}:${String(time.getSeconds()).padStart(2, 0)}`;
 }
 
 /**
