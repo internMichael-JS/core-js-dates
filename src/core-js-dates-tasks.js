86bc0ab2b1c6475c814201d50405eed31d4ea487
Michail <astapenko.m99@gmail.com>
2025-03-05 18:46:35 +0300

    feat: done getNextFriday

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -75,8 +75,10 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const time = new Date(date);
+  time.setDate(time.getDate() + ((5 - time.getDay() + 7) % 7 || 7));
+  return time;
 }
 
 /**
